3b177f09287ae1b2236db45514a9bf5bb6615eb0
Chun Yau <mondez.kyusuke@googlemail.com>
2015-10-06 04:22:24 +0100

    Minor code cleanup

--- src/uk/co/kyusuke/PortalEntityRestriction.java
+++ src/uk/co/kyusuke/PortalEntityRestriction.java
@@ -3,7 +3,6 @@ package uk.co.kyusuke;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
